3f8f3a905f3ad0b84fce4c9944eef12a33bbf19f
neel2911 <neelrokr@gmail.com>
2021-11-29 00:51:23 -0500

    - added css in common.css - created responsive layout for index.html

---
+++ scripts/drawer.js
@@ -0,0 +1,26 @@
+const $ = function (selector) {
+  return document.getElementById(selector);
+};
+
+/* Set the width of the side navigation to 250px */
+const openNav = function () {
+  const navEl = $("navigationBar");
+  navEl.style.transition = "0.5s";
+  navEl.style.width = "150px";
+};
+
+/* Set the width of the side navigation to 0 */
+const closeNav = function () {
+  const navEl = $("navigationBar");
+  navEl.style.width = "0";
+  setTimeout(() => {
+    navEl.style.transition = "none";
+  }, 500);
+};
+
+window.onload = function () {
+  const menuEl = $("menu");
+  const closeDrawerEl = $("close_drawer");
+  menuEl.addEventListener("click", openNav);
+  closeDrawerEl.addEventListener("click", closeNav);
+};
